c5dfe20b4957165ad1da717f4cb98375b19bfdd3
kaikeqp <kaikemice@gmail.com>
2022-05-17 17:05:21 -0300

    feature: adicionado projeto automacao base

---
+++ automacao/automacao/src/main/java/automacao/automacao.java
@@ -0,0 +1,56 @@
+package automacao;
+import org.openqa.selenium.By;
+
+import java.util.List;
+
+import org.openqa.selenium.*;
+
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class automacao {
+	public static void main(String[] args) {
+		
+		
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\xps13\\Documents\\Automacao\\chromedriver_win32\\chromedriver.exe");
+		
+		
+		WebDriver driver = new ChromeDriver();
+
+        // Opening Google
+        driver.get("http://www.google.com.br");
+
+        // q its the search box name/ID
+        WebElement element = driver.findElement(By.name("q"));
+
+        element.sendKeys("Fiap");
+        
+        // Submit the form
+        element.submit();
+
+        // Printing the page title - just for example
+        System.out.println("Page title is: " + driver.getTitle());
+        
+
+       
+       // List<WebElement> findElements = driver.findElements(By.xpath("//*[@id=\"gsr\"]"));
+
+       List<WebElement> findElements = driver.findElements(By.xpath("//*[@id='rso']//h3/a"));
+
+        //Get the url of first link and navigate to it
+        String fisrt_link = findElements.get(0).getAttribute("href");
+        driver.navigate().to(fisrt_link);
+       
+        
+        
+      
+//        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((
+//        
+		// If you want to Close the browser, uncomment this line
+		// driver.quit();
+    }
+}
